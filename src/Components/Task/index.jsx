7afdcb34ae6c1d6f3c90400e86869bc085c93b09
DevFaith <waitherafaith@outlook.com>
2024-06-02 16:52:19 +0300

    * refactor(Task): reformat code for better readability and consistency

--- src/Components/Task/index.jsx
+++ src/Components/Task/index.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import styles from './task.module.css';
 import { FaTrashAlt } from "react-icons/fa";
 import { FaCheckCircle } from "react-icons/fa";
 
 export function Task({ task, onComplete, onDelete }) {
-    return (
-        <div className={styles.task}>
-            <button className={styles.checkContainer} onClick={() => onComplete(task.id)}>
-                {task.isCompleted ? <FaCheckCircle /> : <div />}
-            </button>
-            <p className={task.isCompleted ? styles.textcompleted : ''}>{task.title}</p>
-            <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
-                <FaTrashAlt size={20} />
-            </button>
-        </div>
-    );
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer} onClick={() => onComplete(task.id)}>
+        {task.isCompleted ? <FaCheckCircle /> : <div />}
+      </button>
+      <p className={task.isCompleted ? styles.textcompleted : ''}>{task.title}</p>
+      <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
+        <FaTrashAlt size={20} />
+      </button>
+    </div>
+  );
 }
